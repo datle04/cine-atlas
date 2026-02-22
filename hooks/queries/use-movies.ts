3da2851200a5c98c5f5b87c7d4ade00eaa1395c4
Dat Le <ldat0909@gmail.com>
2026-02-22 15:07:54 +0700

    Added tmdb services

--- hooks/queries/use-movies.ts
+++ hooks/queries/use-movies.ts
@@ -1,34 +1,57 @@
-import { getPopularMovies, getPopularTVShows, getTopRatedMovies, getTrendingMovies } from "@/services/tmdb.service"
+import * as tmdbService from "@/services/tmdb.service";
 import { Movie, TV } from "@/types/tmdb";
 import { useQuery } from "@tanstack/react-query"
 
 export const useTrendingMovies = (initialData?: Movie[]) => {
     return useQuery({
-        queryKey: ["movies", "trending"],
-        queryFn: () => getTrendingMovies(),
+        queryKey: ["movies", "trending","week"],
+        queryFn: () => tmdbService.getTrending('movie', 'week'),
         initialData: initialData, 
     });
 };
 
-export const usePopularMovies = () => {
-    return useQuery({
-        queryKey: ['movies', 'popular'],
-        queryFn: () => getPopularMovies()
-    })
-}
-
-export const useTopRatedMovies = (initialData?: Movie[]) => {
+export const usePopularMovies = (page = 1) => {
   return useQuery({
-    queryKey: ["movies", "top_rated"],
-    queryFn: getTopRatedMovies,
-    initialData: initialData,
+    queryKey: ["movies", "popular", page],
+    queryFn: () => tmdbService.getPopularMovies(page),
   });
 };
 
-export const usePopularTVShows = (initialData?: TV[]) => {
+export const useTopRatedMovies = (page = 1) => {
   return useQuery({
-    queryKey: ["tv", "popular"],
-    queryFn: getPopularTVShows,
-    initialData: initialData,
+    queryKey: ["movies", "top_rated", page],
+    queryFn: () => tmdbService.getTopRatedMovies(page),
+  });
+};
+
+export const useMovieDetails = (id: string | number) => {
+  return useQuery({
+    queryKey: ["movie", "details", id],
+    queryFn: () => tmdbService.getMovieDetails(id),
+    enabled: !!id, // Only called if id is valid
+  });
+};
+
+export const useMovieCredits = (id: string | number) => {
+  return useQuery({
+    queryKey: ["movie", "credits", id],
+    queryFn: () => tmdbService.getMovieCredits(id),
+    enabled: !!id,
+  });
+};
+
+export const useMovieVideos = (id: string | number) => {
+  return useQuery({
+    queryKey: ["movie", "videos", id],
+    queryFn: () => tmdbService.getMovieVideos(id),
+    enabled: !!id,
+  });
+};
+
+export const useSimilarMovies = (id: string | number) => {
+  return useQuery({
+    queryKey: ["movie", "similar", id],
+    queryFn: () => tmdbService.getSimilarMovies(id),
+    enabled: !!id,
   });
 };
